fc894c69c64d9640340b2becb98eaf7579343fec
Alef-Keuffer <alefkeuffer@gmail.com>
2022-05-29 18:54:58 +0100

    Added index generation and clean from compile.sh

--- compile.sh
+++ compile.sh
@@ -3,5 +3,8 @@ filename="cp2021t"
 lhs2TeX "$filename.lhs" > "$filename.tex"
 xelatex -interaction batchmode "$filename"
 bibtex "$filename"
+makeindex "$filename"
 xelatex -interaction batchmode "$filename"
 xelatex -interaction batchmode "$filename"
+
+rm -f *.tex *.log *.synctex.gz *.out *.aux *.bbl *.idx *.ilg *.ind *.ptb *.blg
